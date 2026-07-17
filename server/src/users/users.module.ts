a6171e4e01b407e941a7f92ca75be170d0e9e391
adams <adamskiptalam0@gmail.com>
2026-07-17 18:58:25 +0300

    chore: create functions to find a user by id and by email

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service.js';
 import { UsersController } from './users.controller.js';
+import { PrismaService } from '../prisma.service.js';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
+  exports: [PrismaService]
 })
 export class UsersModule { }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service.js'
 
 @Injectable()
 export class UsersService {
-  register() {
+  constructor(
+    private prisma: PrismaService
+  ) { }
 
+  // find user with email
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email }
+    });
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+    const {
+      passwordHash: _passwordHash,
+      ...result
+    } = user;
+    return result
+  }
+
+  // find user with id
+  async findById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id }
+    });
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+    const {
+      passwordHash: _passwordHash,
+      ...result
+    } = user;
+    return result
   }
 }
