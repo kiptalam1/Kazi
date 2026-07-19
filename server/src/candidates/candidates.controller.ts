a72d3014152b12bc29c422be2372c42da498d5b7
adams <adamskiptalam0@gmail.com>
2026-07-19 21:44:26 +0300

    feat: create handler for updating candidate fields

--- server/src/candidates/candidates.controller.ts
+++ server/src/candidates/candidates.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Patch } from '@nestjs/common';
 import { CandidatesService } from './candidates.service.js';
+import { CandidateUpdateDto } from './dto/update-candidate.dto.js';
+import { CurrentUser } from '../common/decorators/current-user.decorator.js';
+import type { CurrentUserInterface } from '../common/interface/current-user.interface.js';
 
 @Controller('api/v1/candidates')
 export class CandidatesController {
   constructor(private readonly candidatesService: CandidatesService) { }
 
+
+  // update candidate fields;
+  @Patch('me')
+  async updateMe(
+    @CurrentUser() user: CurrentUserInterface,
+    @Body() dto: CandidateUpdateDto,
+  ) {
+    return await this.candidatesService.updateByUserId(user.id, dto)
+  }
+
+  // fetch a candidate by candidate-id
   @Get(':id')
   async findOneCandidate(
     @Param('id', ParseUUIDPipe)
