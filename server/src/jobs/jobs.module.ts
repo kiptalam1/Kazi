18bf038257ef6fa508eeb2b4561d1241ecbf9e58
adams <adamskiptalam0@gmail.com>
2026-08-06 18:16:31 +0300

    fix: consume reusable code

--- server/src/jobs/jobs.module.ts
+++ server/src/jobs/jobs.module.ts
@@ -3,9 +3,10 @@ import { JobsService } from './jobs.service.js';
 import { JobsController } from './jobs.controller.js';
 import { CompaniesService } from '../companies/companies.service.js';
 import { PrismaService } from '../prisma.service.js';
+import { CompanyMembersService } from '../company-members/company-members.service.js';
 
 @Module({
   controllers: [JobsController],
-  providers: [JobsService, CompaniesService, PrismaService],
+  providers: [JobsService, CompaniesService, PrismaService, CompanyMembersService],
 })
 export class JobsModule { }
