36619ae492c5f38f2b40f1f55e35cf9029d198f4
adams <adamskiptalam0@gmail.com>
2026-08-13 18:34:20 +0300

    import logo and favicon

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="border min-h-screen flex flex-col items-center justify-center ">
-      Kazi Landing Page
+      <div>
+        <Image
+          src={"/logo.png"}
+          width={100}
+          height={100}
+          alt="Kazi Logo"
+        />
+      </div>
+      <h1 className="text-xl my-2">Kazi Landing Page</h1>
     </div>
   );
 }
