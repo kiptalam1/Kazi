04a9b477b7dab947886302318de796902c6e5042
adams <adamskiptalam0@gmail.com>
2026-08-15 19:14:33 +0300

    feat: implement hook to fetch authenticated user

---
+++ client/features/auth/api/get-me.ts
@@ -0,0 +1,7 @@
+import { api } from "@/lib/api/client";
+import { type GetMeResponse } from "../types/get-me.types";
+
+export async function getMe(): Promise<GetMeResponse> {
+  const res = await api.get('/users/me');
+  return res.data;
+}
