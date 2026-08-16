666127d6885a9a33d3af250a8c33329a2c50fa69
adams <adamskiptalam0@gmail.com>
2026-08-16 15:33:43 +0300

    --amend

--- client/lib/api/client.ts
+++ client/lib/api/client.ts
@@ -5,3 +5,46 @@ export const api = axios.create({
   withCredentials: true,
   timeoutErrorMessage: 'TimeOut was exceeded.Please try again later.',
 });
+
+
+let isRefreshing = false;
+let pendingRequests: (() => void)[] = [];
+
+api.interceptors.response.use(
+  function(response) {
+    return response;
+  },
+  async function(error) {
+    const originalRequest = error.config;
+    const isUnauthorized = error.response?.status === 401;
+    const isRefreshRequest = originalRequest?.url?.includes(
+      '/auth/refresh-tokens',
+    );
+    if (!isUnauthorized || originalRequest?._retry || isRefreshRequest) {
+      return Promise.reject(error);
+    }
+    originalRequest._retry = true;
+
+    if (isRefreshing) {
+      await new Promise<void>((resolve) => {
+        pendingRequests.push(resolve);
+      });
+      return api(originalRequest);
+    }
+    isRefreshing = true;
+    try {
+      await api.post('/auth/refresh-tokens');
+      pendingRequests.forEach((resolve) => resolve());
+      pendingRequests = [];
+      return api(originalRequest);
+    } catch (refreshError) {
+      pendingRequests = [];
+
+      window.location.href = '/login';
+
+      return new Promise(() => { });
+    } finally {
+      isRefreshing = false;
+    }
+  },
+);
