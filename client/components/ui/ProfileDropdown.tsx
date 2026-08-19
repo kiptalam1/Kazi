8631f1703f0d9a0b59bc0e2f338debffacda67db
adams <adamskiptalam0@gmail.com>
2026-08-19 12:38:52 +0300

    feat: improve dropdown

--- client/components/ui/ProfileDropdown.tsx
+++ client/components/ui/ProfileDropdown.tsx
@@ -1,3 +1,6 @@
+import { LogOut, User } from 'lucide-react';
+import Link from 'next/link';
+
 type ProfileDropdownProps = {
   isOpen: boolean;
   onClose: () => void;
@@ -12,6 +15,19 @@ export default function ProfileDropdown({
   }
 
   return (
-    <div className="absolute z-50 right-0 border border-border bg-white top-full w-60 h-60 mt-2 shadow-xs rounded-sm"></div>
+    <div className="absolute z-50 right-0 border border-border bg-white top-full w-60 h-auto mt-6 shadow-xs flex flex-col">
+      <Link
+        href={'/profile'}
+        className="text-text-secondary flex items-center gap-2 px-4 py-3 hover:bg-background-muted text-sm"
+      >
+        <User className="size-4 " />
+        Profile
+      </Link>
+      <button className="text-danger flex items-center gap-2 t hover:bg-background-muted px-4 py-3 duration-75">
+        {' '}
+        <LogOut className="size-4" />
+        Log Out
+      </button>
+    </div>
   );
 }
--- client/components/ui/TopBar.tsx
+++ client/components/ui/TopBar.tsx
@@ -1,21 +1,37 @@
-'use client';
-
-import { useAuth } from '@/features/auth/hooks/useAuth';
+'use client'; import { useAuth } from '@/features/auth/hooks/useAuth';
 import { CompanyLogo } from './CompanyLogo';
 import { Avatar } from './Avatar';
-import { Bell, BriefcaseBusiness, FileText, User } from 'lucide-react';
+import { Bell, BriefcaseBusiness, FileText, } from 'lucide-react';
 import NavLink from './NavLink';
 import FallbackAvatar from './FallbackAvatar';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ProfileDropdown from './ProfileDropdown';
 
 export default function TopBar() {
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
+  const dropDownRef = useRef<HTMLDivElement>(null);
   const { data, isPending, isError } = useAuth();
+
+  useEffect(() => {
+    function handlePointerDown(event: PointerEvent) {
+      if (dropDownRef.current && !dropDownRef.current.contains(event.target as Node)) {
+        setIsOpenDropdown(false);
+      }
+    }
+
+    if (isOpenDropdown) {
+      document.addEventListener('pointerdown', handlePointerDown);
+    }
+
+    return () => {
+      document.removeEventListener('pointerdown', handlePointerDown)
+    }
+  }, [isOpenDropdown]);
   if (isPending || isError || !data) {
     return null;
   }
   const user = data.data;
+
 
   return (
     <nav className="flex items-center justify-between border-b border-border-muted shadow-xs text-sm px-6 sm:px-8 py-4 sm:py-6">
@@ -41,20 +57,26 @@ export default function TopBar() {
         </NavLink>
       </div>
       <div
+        ref={dropDownRef}
         className="relative"
-        onClick={() => setIsOpenDropdown((prev) => !prev)}
       >
-        {user.avatar ? (
-          <Avatar
-            src={user.avatar}
-            alt="avatar"
-            width={32}
-            height={32}
-            className="w-auto h-auto"
-          />
-        ) : (
-          <FallbackAvatar value={user.firstName} className="size-10" />
-        )}
+        <button
+          onClick={() => setIsOpenDropdown(
+            (prev) => !prev)}
+          className='cursor-pointer'
+        >
+          {user.avatar ? (
+            <Avatar
+              src={user.avatar}
+              alt="avatar"
+              width={32}
+              height={32}
+              className="w-auto h-auto"
+            />
+          ) : (
+            <FallbackAvatar value={user.firstName} className="size-10" />
+          )}
+        </button>
         {/* dropdown */}
         {isOpenDropdown && (
           <ProfileDropdown
