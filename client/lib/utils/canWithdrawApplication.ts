da6a1a30d92eb28f005a9b032efca04bde396b68
adams <adamskiptalam0@gmail.com>
2026-08-21 12:56:35 +0300

    create function to determine withdraw permission

---
+++ client/lib/utils/canWithdrawApplication.ts
@@ -0,0 +1,12 @@
+import type { ApplicationStatus } from "@/features/common/types/common.types";
+
+const withdrawableStatuses: ApplicationStatus[] = [
+  'PENDING',
+  'REVIEWING',
+  'INTERVIEW',
+  'SHORTLISTED',
+];
+
+export function canWithdrawApplication(status: ApplicationStatus) {
+  return withdrawableStatuses.includes(status);
+}
