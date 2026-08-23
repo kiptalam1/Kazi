ea58488cffc6041027446f452da5b84edf1a65f5
adams <adamskiptalam0@gmail.com>
2026-08-23 17:27:12 +0300

    feat: display candidate resumes in the select tag

--- client/features/jobs/components/ApplyForm.tsx
+++ client/features/jobs/components/ApplyForm.tsx
@@ -2,15 +2,17 @@ import Button from "@/components/ui/Button"
 import Label from "@/components/ui/Label"
 import Select from "@/components/ui/Select"
 import Textarea from "@/components/ui/Textarea"
+import { useCandidateResumes } from "@/features/candidates/hooks/useCandidateResumes";
 
 type Props = {
   onClose: () => void;
 };
 export const ApplyForm = ({ onClose }: Props) => {
+  const { data: resumes, isPending: isPendingResume, isError: isErrorResume, } = useCandidateResumes();
   return (
     <form
       onClick={(e) => e.stopPropagation()}
-      className="p-4 sm:p-6 md:p-8 bg-background w-full max-w-lg space-y-6">
+      className="p-4 sm:p-6 md:p-8 bg-background w-full max-w-lg space-y-6 shadow-sm">
       <h2
         id="apply-modal-title"
         className="font-semibold"
@@ -19,16 +21,47 @@ export const ApplyForm = ({ onClose }: Props) => {
       </h2>
       <div className="flex flex-col gap-1">
         <Label htmlFor="resume">Resume</Label>
-        <Select
-          id="resume"
-          name="resumeId"
-          defaultValue=""
-        >
-          <option value="" disabled>
-            Select a resume
-          </option>
-        </Select>
+        {
+          isPendingResume ? (
+            <div className="text-xs text-text-muted">
+              Loading resume...
+            </div>
+          ) : isErrorResume ? (
+            <div className="text-xs text-text-muted">
+              Unable to load resumes.
+            </div>
+          ) :
+            resumes && resumes.length > 0 ? (
+              <Select
+                id="resume"
+                name="resumeId"
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  No resume
+                </option>
+
+                {resumes.map((resume) => (
+                  <option key={resume.id}
+                    value={resume.id}
+                  >
+                    {resume.displayName || resume.fileName}
+                  </option>
+                ))}
+              </Select>
+            ) : (
+              <div className="flex items-center justify-between gap-4 text-xs text-text-muted">
+                <span>No resume uploaded</span>
+                <Button
+                  variant='basic'
+                  type="button"
+                  className="text-xs">
+                  Upload
+                </Button>
+              </div>
+            )}
       </div>
+
       <div className="flex flex-col gap-1">
         <Label htmlFor='coverLetter'>
           Cover Letter
@@ -38,6 +71,13 @@ export const ApplyForm = ({ onClose }: Props) => {
           name="coverLetter"
           className="min-h-48"
           placeholder="Tell the employer why you're a good fit for this position..."
+          rows={8}
+          maxLength={5000}
+          spellCheck
+          autoCorrect="on"
+          autoCapitalize="sentences"
+          autoComplete="off"
+          wrap="soft"
         />
       </div>
       <div className='flex justify-end gap-4 items-center text-sm'>
