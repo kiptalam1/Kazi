100c775f067d7fd1fa0d9ccdc01ad3f46a54494e
adams <adamskiptalam0@gmail.com>
2026-08-31 15:49:55 +0300

    Add link for employers

--- client/components/ui/TopBar.tsx
+++ client/components/ui/TopBar.tsx
@@ -59,6 +59,11 @@ export default function TopBar() {
           <Bell size={20} className="sm:hidden" />
           <span className="hidden sm:block">Notifications</span>
         </NavLink>
+        <NavLink href={'/employer'}>
+          <Bell size={20} className="sm:hidden" />
+          <span className="hidden sm:block">For Employers</span>
+        </NavLink>
+
       </div>
       <div ref={dropDownRef} className="relative">
         <button
