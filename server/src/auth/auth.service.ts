3809a2f68e700d35335fcee36f89eabca200f369
adams <adamskiptalam0@gmail.com>
2026-09-19 14:17:02 +0300

    fix: change cookie settings to samesite-none

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -238,7 +238,7 @@ export class AuthService {
     const REFRESH_TOKEN_MAX_AGE = 7 * 24 * 60 * 60 * 1000;
     const commonOptions = {
       secure: process.env.NODE_ENV == 'production',
-      sameSite: 'lax' as const,
+      sameSite: 'none' as const,
       httpOnly: true,
       // partitioned: true,
       path: '/',
