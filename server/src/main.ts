6d971f16fe8806e188632b5c4ac2fbcd8d740c0e
adams <adamskiptalam0@gmail.com>
2026-09-23 16:49:56 +0300

    fix: ensure notifications are ordered from last to first

--- server/src/main.ts
+++ server/src/main.ts
@@ -21,10 +21,8 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
-    // origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
-
   });
 
   const swaggerConfig = new DocumentBuilder()
